65f0e64ce58d53e5e66bba39ae6c77ff0785b8ec
Austin Ivicevic <austin.ivicevic@outlook.com>
2015-09-29 16:46:03 -0700

    Last commit missed this file

---
+++ King's_Truth_v2.0/Tile.h
@@ -0,0 +1,52 @@
+#ifndef TILE_H
+#define TILE_H
+#include "stdafx.h"
+
+// Tile Size
+#define TILE_SIZE_X 64
+#define TILE_SIZE_Y 64
+
+// Tile types
+enum Tile_Types
+{
+	PATH = 0,
+	PATH_2 = 1,
+	SINGLE_WALL_1 = 2,
+	SINGLE_WALL_2 = 3,
+	SINGLE_WALL_3 = 4,
+	SINGLE_WALL_4 = 5,
+	SINGLE_WALL_5 = 6,
+	DOUBLE_WALL_1 = 7,
+	DOUBLE_WALL_2 = 8,
+	DOUBLE_WALL_3 = 9,
+	LONG_WALL_X = 10,
+	LONG_WALL_Y = 11
+};
+
+class Tile
+{
+protected:
+	RECT sourceRect;
+
+public:
+	Tile();
+	virtual ~Tile();
+
+	RECT GetSourceRect();
+};
+
+
+class SingleTile : public Tile
+{
+public:
+	SingleTile();
+};
+
+
+class PathTile : public Tile
+{
+public:
+	PathTile();
+};
+
+#endif
